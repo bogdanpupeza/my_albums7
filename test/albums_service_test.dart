0c027c76a1bc3f8facdb9d082b1eeb946b8ea1ca
bogdanpupeza <bogdan.pupeza@rodeapps.com>
2021-09-15 16:24:10 +0300

    Update albums_service_test.dart

--- test/albums_service_test.dart
+++ test/albums_service_test.dart
@@ -1,20 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:my_albums6/model/albums.dart';
 import 'package:my_albums6/model/albums_service.dart';
 
 void main(){
   AlbumsService albumsService = AlbumsService();
-  List<Album> albums = [
-    Album(
-      id: 1,
-      userId: 1,
-      name: "AAA",
-    ),
-  ];
-  test("Test if we get an error", (){
+  List<int> albums = [];
+  for(int i = 1; i <= 100; ++i)
+  {
+    albums.add(i);
+  }
+  test("Test if we get albums in order", (){
     expect(
-      albumsService.getAlbums(), 
-      emits(albums),
+      albumsService.getAlbums().map(
+        (albumsData){
+          return albumsData.map(
+            (album){
+              return album.id;
+            }
+          ).toList();
+        }
+      ), 
+      emits(albums)
     );
   });
 }
