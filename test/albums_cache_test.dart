ec60d6b068ae356b97e06282d9fe9ddc33a0147f
bogdanpupeza <bogdan.pupeza@rodeapps.com>
2021-09-20 17:45:41 +0300

    albums_cache

---
+++ test/albums_cache_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:my_albums6/model/albums.dart';
+import 'package:my_albums6/model/albums_cache.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'albums_cache_test.mocks.dart';
+
+@GenerateMocks([SharedPreferences])
+void main(){
+  List<Album> albums = [];
+  for(int i = 0; i < 100; ++i){
+    albums.add(
+      Album(
+        id: i,
+        userId: i % 33,
+        name: "$i",
+        favorite: i % 2 == 0,
+      )
+    );
+  }
+  // test("Test if we get an error", (){
+    
+  //   expect(
+  //     albumsCache.getAlbums(), 
+  //     emits(albums),
+  //   );
+    
+  // });
+}
