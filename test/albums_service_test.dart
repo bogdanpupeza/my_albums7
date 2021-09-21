b6be5ac0b1d8964cf9eb0dffc1f398cf80bbfd31
bogdanpupeza <bogdan.pupeza@rodeapps.com>
2021-09-21 10:55:37 +0300

    update1

--- test/albums_service_test.dart
+++ test/albums_service_test.dart
@@ -1,19 +1,39 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:my_albums6/model/albums.dart';
 import 'package:my_albums6/model/albums_service.dart';
+import 'package:http/http.dart' as http;
 
-void main(){
+import 'albums_service_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  final client = MockClient();
+  final String _url = "https://jsonplaceholder.typicode.com/albums";
   AlbumsService albumsService = AlbumsService();
-  List<Album> albums = [
-    Album(
-      id: 1,
-      userId: 1,
-      name: "AAA",
-    ),
-  ];
-  test("Test if we get an error", (){
+  List<Album> albums = [];
+  for (int i = 0; i < 100; ++i) {
+    albums.add(Album(
+      userId: i % 20,
+      name: "album $i",
+      id: i,
+      favorite: i % 2 == 0,
+    ));
+  }
+  String jsonAlbums = json.encode(
+    albums.map((album){
+      return album.toJson();
+    }
+  ).toList());
+  Response response = Response(jsonAlbums,200);
+  test("Test if we get albums", () {
+    when(client.get(Uri.parse(_url))).thenAnswer((_)=>Future.value(response));
     expect(
-      albumsService.getAlbums(), 
+      albumsService.getAlbums(),
       emits(albums),
     );
   });
